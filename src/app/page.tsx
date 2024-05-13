a3a0d7496084ee776e833cd07ff4ee27152b3d6d
tonmoy-dev <rxdevs100@gmail.com>
2024-05-13 23:00:35 +0600

    feat: Add why choose us section in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import HeroSection from "@/components/HeroSection";
 import FeaturedCourses from "@/components/FeaturedCourses";
 import Image from "next/image";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.2]">
       <HeroSection />
       <FeaturedCourses />
+      <WhyChooseUs />
     </main>
   );
 }
