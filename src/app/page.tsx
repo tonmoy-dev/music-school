429dd58aedb00992589aa457c48887a96d709877
tonmoy-dev <rxdevs100@gmail.com>
2024-05-13 20:54:40 +0600

    feat: Add Hero section to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import HeroSection from "@/components/HeroSection";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.2]">
-      <h1>Music School!</h1>
+      <HeroSection />
     </main>
   );
 }
