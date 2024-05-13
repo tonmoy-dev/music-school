e7a2ffcccf087d4b71de760a0124af3653370653
tonmoy-dev <rxdevs100@gmail.com>
2024-05-13 21:43:12 +0600

    feat: Add Featured courses section to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import HeroSection from "@/components/HeroSection";
+import FeaturedCourses from "@/components/FeaturedCourses";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.2]">
       <HeroSection />
+      <FeaturedCourses />
     </main>
   );
 }
